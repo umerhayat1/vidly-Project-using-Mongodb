f76a5574bf9247beb3b3ede05c1428f661acc15b
John Doe <umerhayat.tech@gmail.com>
2021-10-25 11:12:36 +0500

    Before Unit Test

--- routes/genres.js
+++ routes/genres.js
@@ -7,8 +7,6 @@ const { Genre, validate } = require('../models/genre')
 const express = require('express');
 const router = express.Router();
 
-
-
 router.get('/', async (req, res) => {
   // throw new Error('could not get the genres')
   const genres = await Genre.find().sort('name')
--- startup/config.js
+++ startup/config.js
@@ -4,6 +4,5 @@ module.exports = function () {
     //execute if jwtPrivatekey environment variable is not set
     if (!config.get('jwtPrivateKey')) {
         throw new Error('FATAL ERROR: jwtPrivateKey is not defined');
-
     }
 }
