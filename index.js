aadc693bf4ea38abd90ef652c80501e939b63b5b
John Doe <umerhayat.tech@gmail.com>
2021-10-27 12:20:45 +0500

    Ready for Deployment

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ require('./startup/routes')(app)
 require('./startup/db')()
 require('./startup/config')()
 require('./startup/validation')()
+require('./startup/prod')(app)
 
-
-const port = app.set(process.env.PORT || 3000);
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-// const port = process.env.PORT || 3000;
+// const port = app.set(process.env.PORT || 3000);
 // const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
